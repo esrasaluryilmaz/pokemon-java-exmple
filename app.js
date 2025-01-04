56aeeebfd5969f3b6029b4109dc0d490163d2832
esrasaluryilmaz <esrasaluresrasalur@gmail.com>
2025-01-04 22:08:49 +0100

    ilerleme

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const poke_container = document.querySelector(".poke-container");
+const search = document.querySelector(".search");
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document.querySelector(".searchInput");
+
+const pokemon_count = 151;
+
 const bg_color = {
   grass: "#8BD369",
   fire: "#FF603F",
@@ -15,3 +22,21 @@ const bg_color = {
   dragon: "#7766EE",
   ice: "#66CCFF",
 };
+
+searchBtn.addEventListener("click", () => {
+  search.classList.toggle("active");
+});
+
+const fetchPokemons = async () => {
+  for (let i = 1; i < pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetchPokemons(url);
+  const data = await res.json();
+};
+
+fetchPokemons();
